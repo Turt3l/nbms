2562bda68da33e22c41ce0060ac4c1af1265c60c
Turt3l <77460157+Turt3l@users.noreply.github.com>
2022-12-19 20:37:08 +0200

    initial commit

---
+++ src/sections/landingPage/LandingPage.jsx
@@ -0,0 +1,40 @@
+
+import logo from "../../media/images/logo.webp"
+function LandingPage() {
+
+    return(
+        <div className="LandingContainer">
+            <div className="LandingWrapper">
+                <div className="col text-col">
+                    <div className="headerLogoContainer">
+                        <div className="logo">
+                            <img src={logo}/>
+                        </div>
+                    </div>
+                    <div className="textContainer">
+                        <div className="textWrapper">
+                            <div className="slide-up mainText">
+                                <h1>Breath-taking design for your business</h1>
+                            </div>
+                            <div className="slide-up-slow descriptionText">
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem repellat, distinctio amet consequunt</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="scrolldownContainer">
+
+                    </div>
+                </div>
+                <div className="col image-col">
+                    <div className="overlay">
+                        <div className="imageSliderContainer">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default LandingPage;
