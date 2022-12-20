5fd06c403ad9d665c54ce73ee07b1529d2e3de9a
KanKan-coding <markusskancans05@gmail.com>
2022-12-20 20:22:32 +0200

    created perfectweb sections

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import LandingPage from "./sections/landingPage/LandingPage";
+import WhatDoWeDo from "./sections/wdwdPage/Wdwd";
+import PerfectWeb from "./sections/perfectweb/perfectweb";
+function App() {
+  setInterval(function(){
+    document.getElementById("appContainer").classList.add("visible")
+  }, 200)
+  return (
+    <div className="App">
+      <div id="appContainer">
+        <LandingPage/>
+        <WhatDoWeDo/>
+        <PerfectWeb/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
